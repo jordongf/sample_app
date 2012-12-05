54225ca7157183be372e6b8d77b813ffbf583805
Jordon Foerster <jordongf@gmail.com>
2012-12-05 14:26:18 -0800

    blah

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,12 +25,27 @@ describe "UserPages" do
     let(:submit) { "Create my account" }
     
     describe "with invalid information" do
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content ('error') }
+      end
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
     
     describe "with valid information" do
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
+      
       before do
         fill_in "Name",          with: "Example User"
         fill_in "Email",         with: "user@example.com"
