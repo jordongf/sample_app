67681fd1d62c7bba965af7dfa530180273283d6e
Jordon Foerster <jordongf@gmail.com>
2012-12-09 11:43:03 -0800

    finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
   
   subject { page }
   
@@ -38,14 +38,6 @@ describe "UserPages" do
     
     describe "with valid information" do
       
-      describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by_email('user@example.com') }
-        
-        it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-        end
-      
       before do
         fill_in "Name",          with: "Example User"
         fill_in "Email",         with: "user@example.com"
@@ -54,6 +46,15 @@ describe "UserPages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+  
+     describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+                
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
