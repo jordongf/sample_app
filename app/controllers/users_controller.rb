2cd71863ec58381c4d7bc165ce346ff886c3c2cf
Jordon Foerster <jordongf@gmail.com>
2012-12-12 22:39:04 -0800

    9.0 - 9.2 tests passed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-    
+     
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -18,4 +18,19 @@ class UsersController < ApplicationController
       render 'new'
       end
     end
+  
+  def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user      
+    else
+      render 'edit'
+    end
+  end
+end
